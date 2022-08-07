890ce778d6ab3f00fee7e87cf1691436da3a9d67
NickyKlaus <nickolai.beloglazov@gmail.com>
2022-08-07 17:58:29 +0600

    Basics#1

--- hello.go
+++ hello.go
@@ -12,36 +12,34 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.Black, color.CMYK{218, 6, 218, 0}}
+var palette = []color.Color{color.Black, color.CMYK{C: 218, M: 6, Y: 218}}
 
-const (
-	colorIndex = 1
-	blackIndex = 0
-)
+const colorIndex = 1
 
 func main() {
-	out_file, err := os.Create("./out.data")
+	outFile, err := os.Create("./out.data")
+	if err == nil {
+		err = lissajous(outFile)
+	}
 	if err != nil {
 		fmt.Printf("Error: %s", err)
-	} else {
-		lissajous(out_file)
 	}
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer) error {
 	const (
-		cycles  = 10
-		res     = 0.001
-		size    = 320
-		nframes = 24
-		delay   = 8
+		cycles = 10
+		res    = 0.001
+		size   = 320
+		frames = 24
+		delay  = 8
 	)
 
 	rand.Seed(time.Now().UTC().UnixNano())
 	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
+	anim := gif.GIF{LoopCount: frames}
 	phase := 0.0
-	for i := 0; i < nframes; i++ {
+	for i := 0; i < frames; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
@@ -53,5 +51,5 @@ func lissajous(out io.Writer) {
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim)
+	return gif.EncodeAll(out, &anim)
 }
